985131019206f97cfc05566655679cc728ba6a2f
road24 <road_24_4@hotmail.com>
2019-05-05 21:42:40 -0500

    Added License
    Added license to source code files

--- ChidaTuPrexLoader/ChidaTuPrexLoader/stdafx.h
+++ ChidaTuPrexLoader/ChidaTuPrexLoader/stdafx.h
@@ -10,7 +10,3 @@
 #define WIN32_LEAN_AND_MEAN             // Excluir material rara vez utilizado de encabezados de Windows
 // Archivos de encabezado de Windows:
 #include <windows.h>
-
-
-
-// TODO: mencionar aquí los encabezados adicionales que el programa necesita
